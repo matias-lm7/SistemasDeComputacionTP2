24ad692a70df4430b8fa9ce3e70fd6684688b911
matias <matias-morales-422003@mi.unc.edu.ar>
2026-04-17 16:51:56 -0300

    Delete Primera_Iteracion/src directory

--- Primera_Iteracion/src/main.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-
-void convertion(float* input, int* output, int length) {
-    for (int i = 0; i < length; i++) {
-        output[i] = (int)(input[i] + 1.0f);
-    }
-}
